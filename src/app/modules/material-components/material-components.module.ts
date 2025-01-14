38c9dc9d93e2b3a74c0904446c6134792ce7f85e
richard ocaranza <kodenook@proton.me>
2025-01-14 00:42:10 -0300

    feat(layout): add application header with title and icon :sparkles:
    implements basic header component with brand name and task icon

--- src/app/modules/material-components/material-components.module.ts
+++ src/app/modules/material-components/material-components.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
   ],
-  exports: []
+  exports: [
+    MatIconModule,
+  ]
 })
-export class MaterialComponentsModule { }
+export class MaterialComponentsModule {}
