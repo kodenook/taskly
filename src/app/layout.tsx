ab96a344eddee9dc524348132778d07a28d9c456
richard ocaranza <richard.ocaranza@proton.me>
2025-06-04 15:52:12 -0400

    feat: Initial :tada:
    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next'
+
+import './globals.css'
+import { AntdRegistry } from '@ant-design/nextjs-registry'
+import '@ant-design/v5-patch-for-react-19'
+
+export const metadata: Metadata = {
+    description: 'Organize and manage your tasks quickly and easily.',
+    title: 'Taskly',
+}
+
+const RootLayout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) => {
+    return (
+        <html lang="en">
+            <AntdRegistry>
+                <body>
+                    {children}
+                </body>
+            </AntdRegistry>
+        </html>
+    )
+}
+
+export default RootLayout
